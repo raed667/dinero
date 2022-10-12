705b2135200f34f2591cef65e51462a6988b6137
Raed Chammam <raed.chammam@ricardo.ch>
2022-10-12 21:44:34 +0200

    initial commit

---
+++ core/src/messages.rs
@@ -0,0 +1,6 @@
+pub const INVALID_INPUT: &str = "Input is invalid.";
+// pub const INVALID_AMOUNT_MESSAGE: &str = "Amount is invalid.";
+// pub const INVALID_SCALE_MESSAGE: &str = "Scale is invalid.";
+// pub const INVALID_RATIOS_MESSAGE: &str = "Ratios are invalid.";
+// pub const UNEQUAL_SCALES_MESSAGE: &str = "Objects must have the same scale.";
+pub const UNEQUAL_CURRENCIES_MESSAGE: &str = "Objects must have the same currency.";
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use core::{add, Dinero};
+
+use currencies::USD;
+
+// The project probably won't have a binary, this is just for quick testing
+
+fn main() {
+    let d1 = Dinero::new(420, USD, Some(2));
+    let d2 = Dinero::new(300, USD, Some(3));
+
+    let d3 = add(&d1, &d2);
+
+    match d3 {
+        Ok(result) => println!("d3 {:?}\n\n", result),
+        Err(e) => println!("Error {}", e),
+    }
+}
